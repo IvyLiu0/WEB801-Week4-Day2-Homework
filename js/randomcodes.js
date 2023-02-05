941b3eb12c264f49dc05fa677d2cb9eb3043814d
Ying_Y <ziyingliu@Ivyyyys-MacBook-Pro.local>
2023-02-04 22:03:28 -0800

    fix the problem why the random code cannot show

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -20,15 +20,15 @@ document.getElementById("codes").innerHTML=generateCode();
 
 //determine when to able or disable button
 function disableButton(btnvalue){
-    document.getElementById("sumbit").disabled= btnvalue; //able/disable button
+    document.getElementById("submit").disabled= btnvalue; //able/disable button
     if (btnvalue == true) { //test if button disabled or enabled
         //set button and label color translucent
-        document.getElementById("sumbit").style.backgroundColor = "rgba(73,119,209,0.3)";
-        document.getElementById("sumbit").style.color = "rgba(255,255,255,0.5)";
+        document.getElementById("submit").style.backgroundColor = "rgba(73,119,209,0.3)";
+        document.getElementById("submit").style.color = "rgba(255,255,255,0.5)";
     } else {
         //set button and label color with no transparency
         document.getElementById("submit").style.backgroundColor = "rgba(73,119,209,1)";
-        document.getElementById("sumbit").style.color = "rgba(255,255,255,1)";
+        document.getElementById("submit").style.color = "rgba(255,255,255,1)";
     }
 }
 
